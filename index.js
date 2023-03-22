c69610362ebc04f3933706bd06940ed88214a600
k1r0s <ciroreed@gmail.com>
2023-03-22 16:45:50 +0100

    fix some errors and method signatures

--- index.js
+++ index.js
@@ -1,39 +1,62 @@
 const net = require("net");
 
+const EMPTY_STR = d => !!d;
+
 const self = function (host, key) {
   this.host = host;
   // this.key = key; still unused
 }
 
-self.prototype.tap = function (name, change, fromTs) {
+self.prototype.tap = function (name, ...aux) {
+  let fromTs, change;
+
+  if (typeof aux[0] === "number" && typeof aux[1] === "function") {
+    fromTs = aux[0];
+    change = aux[1];
+  } else {
+    fromTs = null;
+    change = aux[0];
+  }
+
   const client = new net.Socket();
   const args = ["tap", name];
   if (fromTs) args.push(fromTs);
   const command = args.join();
   client.connect(1337, this.host);
   client.on("connect", () => client.write(command));
-  client.on("data", d => change(d.toString()));
+  client.on("data", d => d.toString().split("\n").filter(EMPTY_STR).forEach(line => change(line)));
   return () => client.destroy();
 }
 
-self.prototype.fetch = function (name, fromTs, toTs) {
+self.prototype.fetch = function (name, fromTs, toTs, newLineCbk) {
   const client = new net.Socket();
   const args = ["fetch", name, fromTs, toTs];
   const command = args.join();
   client.connect(1337, this.host);
   client.on("connect", () => client.write(command));
   const buffer = [];
-  client.on("data", d => buffer.push(d.toString()));
-  return new Promise(resolve => client.on("end", () => resolve(buffer)));
+  client.on("data", d => buffer.push.apply(buffer, d.toString().split("\n").filter(EMPTY_STR)));
+  if (typeof newLineCbk === "function") {
+    client.on("end", () => buffer.forEach(newLineCbk));
+  } else {
+    return new Promise(resolve => client.on("end", () => resolve(buffer)));
+  }
 }
 
-self.prototype.value = function (name) {
+self.prototype.value = function (name, newLineCbk) {
   const client = new net.Socket();
-  const args = ["fetch", name];
+  const args = ["value", name];
   const command = args.join();
   client.connect(1337, this.host);
   client.on("connect", () => client.write(command));
-  return new Promise(resolve => client.on("data", d => resolve(d.toString())));
+  let result = null;
+  client.on("data", d => result = d.toString().trim());
+  if (typeof newLineCbk === "function") {
+    client.on("end", () => newLineCbk(result));
+  } else {
+    return new Promise(resolve => client.on("end", () => resolve(result)));
+  }
+
 }
 
 module.exports = self;
