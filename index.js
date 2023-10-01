6aeeefbfe78bb3c82011f64f648a2734a6ee35d8
k1r0s <ciroreed@gmail.com>
2023-10-01 14:40:01 +0200

    added optional encryption

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const net = require("net");
+const crypto = require("crypto");
+
+function encrypt(text, key) {
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv("aes-256-cbc", Buffer.from(key), iv);
+  let encrypted = cipher.update(text, "utf-8", "hex");
+  encrypted += cipher.final("hex");
+  const combinedData = iv.toString("hex") + encrypted;
+  return combinedData;
+}
 
 const EMPTY_STR = d => !!d;
 
-const self = function (host) {
+function writeCommand (client, command, key) {
+  if (key) {
+    const encryptedData = encrypt(command, key);
+    client.write(encryptedData);
+  } else {
+    client.write(command);
+  }
+}
+
+const self = function (host, key) {
   this.host = host;
+  this.key = key;
 }
 
 self.prototype.tap = function (name, ...aux) {
@@ -22,7 +42,7 @@ self.prototype.tap = function (name, ...aux) {
   if (fromTs) args.push(fromTs);
   const command = args.join();
   client.connect(1337, this.host);
-  client.on("connect", () => client.write(command));
+  client.on("connect", () => writeCommand(client, command, this.key));
   client.on("data", d => d.toString().split("\n").filter(EMPTY_STR).forEach(line => change(line)));
   return () => client.destroy();
 }
@@ -32,7 +52,7 @@ self.prototype.fetch = function (name, fromTs, toTs, newLineCbk) {
   const args = ["fetch", name, fromTs, toTs];
   const command = args.join();
   client.connect(1337, this.host);
-  client.on("connect", () => client.write(command));
+  client.on("connect", () => writeCommand(client, command, this.key));
   const buffer = [];
   client.on("data", d => buffer.push.apply(buffer, d.toString().split("\n").filter(EMPTY_STR)));
   if (typeof newLineCbk === "function") {
@@ -47,7 +67,7 @@ self.prototype.value = function (name, newLineCbk) {
   const args = ["value", name];
   const command = args.join();
   client.connect(1337, this.host);
-  client.on("connect", () => client.write(command));
+  client.on("connect", () => writeCommand(client, command, this.key));
   let result = null;
   client.on("data", d => result = d.toString().trim());
   if (typeof newLineCbk === "function") {
@@ -63,7 +83,7 @@ self.prototype.last = function (name, amount, newLineCbk) {
   const args = ["last", name, amount];
   const command = args.join();
   client.connect(1337, this.host);
-  client.on("connect", () => client.write(command));
+  client.on("connect", () => writeCommand(client, command, this.key));
   const buffer = [];
   client.on("data", d => buffer.push.apply(buffer, d.toString().split("\n").filter(EMPTY_STR)));
   if (typeof newLineCbk === "function") {
