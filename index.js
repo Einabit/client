d061ac4e4212e0fc8e8261af1d99a6ebd653f51a
k1r0s <ciroreed@gmail.com>
2023-10-10 16:32:25 +0200

    added EOL to limit TCP messages

--- index.js
+++ index.js
@@ -12,13 +12,11 @@ function encrypt(text, key) {
 
 const EMPTY_STR = d => !!d;
 
-function writeCommand (client, command, key) {
-  if (key) {
-    const encryptedData = encrypt(command, key);
-    client.write(encryptedData);
-  } else {
-    client.write(command);
-  }
+function writeCommand (socket, command, key) {
+  let auxstr = command;
+  if (key) auxstr = encrypt(command, key);
+
+  socket.write(auxstr + "\n");
 }
 
 const self = function (host, key) {
@@ -37,59 +35,59 @@ self.prototype.tap = function (name, ...aux) {
     change = aux[0];
   }
 
-  const client = new net.Socket();
+  const socket = new net.Socket();
   const args = ["tap", name];
   if (fromTs) args.push(fromTs);
   const command = args.join();
-  client.connect(1337, this.host);
-  client.on("connect", () => writeCommand(client, command, this.key));
-  client.on("data", d => d.toString().split("\n").filter(EMPTY_STR).forEach(line => change(line)));
-  return () => client.destroy();
+  socket.connect(1337, this.host);
+  socket.on("connect", () => writeCommand(socket, command, this.key));
+  socket.on("data", d => d.toString().split("\n").filter(EMPTY_STR).forEach(line => change(line)));
+  return () => socket.destroy();
 }
 
 self.prototype.fetch = function (name, fromTs, toTs, newLineCbk) {
-  const client = new net.Socket();
+  const socket = new net.Socket();
   const args = ["fetch", name, fromTs, toTs];
   const command = args.join();
-  client.connect(1337, this.host);
-  client.on("connect", () => writeCommand(client, command, this.key));
+  socket.connect(1337, this.host);
+  socket.on("connect", () => writeCommand(socket, command, this.key));
   const buffer = [];
-  client.on("data", d => buffer.push.apply(buffer, d.toString().split("\n").filter(EMPTY_STR)));
+  socket.on("data", d => buffer.push.apply(buffer, d.toString().split("\n").filter(EMPTY_STR)));
   if (typeof newLineCbk === "function") {
-    client.on("end", () => buffer.forEach(newLineCbk));
+    socket.on("end", () => buffer.forEach(newLineCbk));
   } else {
-    return new Promise(resolve => client.on("end", () => resolve(buffer)));
+    return new Promise(resolve => socket.on("end", () => resolve(buffer)));
   }
 }
 
 self.prototype.value = function (name, newLineCbk) {
-  const client = new net.Socket();
+  const socket = new net.Socket();
   const args = ["value", name];
   const command = args.join();
-  client.connect(1337, this.host);
-  client.on("connect", () => writeCommand(client, command, this.key));
+  socket.connect(1337, this.host);
+  socket.on("connect", () => writeCommand(socket, command, this.key));
   let result = null;
-  client.on("data", d => result = d.toString().trim());
+  socket.on("data", d => result = d.toString().trim());
   if (typeof newLineCbk === "function") {
-    client.on("end", () => newLineCbk(result));
+    socket.on("end", () => newLineCbk(result));
   } else {
-    return new Promise(resolve => client.on("end", () => resolve(result)));
+    return new Promise(resolve => socket.on("end", () => resolve(result)));
   }
 
 }
 
 self.prototype.last = function (name, amount, newLineCbk) {
-  const client = new net.Socket();
+  const socket = new net.Socket();
   const args = ["last", name, amount];
   const command = args.join();
-  client.connect(1337, this.host);
-  client.on("connect", () => writeCommand(client, command, this.key));
+  socket.connect(1337, this.host);
+  socket.on("connect", () => writeCommand(socket, command, this.key));
   const buffer = [];
-  client.on("data", d => buffer.push.apply(buffer, d.toString().split("\n").filter(EMPTY_STR)));
+  socket.on("data", d => buffer.push.apply(buffer, d.toString().split("\n").filter(EMPTY_STR)));
   if (typeof newLineCbk === "function") {
-    client.on("end", () => buffer.forEach(d => newLineCbk(d)));
+    socket.on("end", () => buffer.forEach(d => newLineCbk(d)));
   } else {
-    return new Promise(resolve => client.on("end", () => resolve(buffer)));
+    return new Promise(resolve => socket.on("end", () => resolve(buffer)));
   }
 
 }
